b6cd17dd2bcdf999c19b2b370a43f517110f5ee8
titicuevas <enrique.cuevas@iesdonana.org>
2022-11-08 19:57:48 +0100

    Actualizacion de la base de datos

--- tienda.sql
+++ tienda.sql
@@ -1,18 +1,16 @@
+-- Active: 1667844048071@@127.0.0.1@5432@tienda
 DROP TABLE IF EXISTS articulos CASCADE;
 
-CREATE TABLE
-    articulos (
-        id bigserial PRIMARY KEY,
-        codigo varchar (13) NOT NULL UNIQUE,
-        descripcion varchar (255) NOT NULL,
-        precio numeric (7, 2) NOT NULL,
-    );
+CREATE TABLE articulos (
+    id          bigserial     PRIMARY KEY,
+    codigo      varchar(13)   NOT NULL UNIQUE,
+    descripcion varchar(255)  NOT NULL,
+    precio      numeric(7, 2) NOT NULL
+);
 
+-- Carga inicial de datos de prueba:
 
-    --Carga inicial de datos de prueba:
-
-    INSERT INTO articulos(codigo, descripcion, precio)
-        VALUES('1231231231231', 'Yogur piña',200),
-                ('4235231231231', 'Tigreton',50.10),
-                ('3234231231231', 'SSD',250);
-                
+INSERT INTO articulos (codigo, descripcion, precio)
+    VALUES ('18273892389', 'Yogur piña', 200.50),
+           ('83745828273', 'Tigretón', 50.10),
+           ('51786128495', 'Disco duro SSD 500 GB', 150.30);
